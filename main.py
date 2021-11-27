5f6129160813d19c0f10376b5daf8e878c280d67
EyeceScream <jhb1085@gmail.com>
2021-11-27 15:17:36 -0600

    Polishing Player creation to have stats for combat.

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 # Player creation
+import random
+
+
 def player_creator():
-    name = input("What is your name?:\n")
-    if 2 < len(name) > 16:
-        print("Naming Rules:\n1: Less than 16 characters and more than 2.")
-        return
-    Player.name = str(name)
-    sex = input("Pick a sex:\n"
-                "M: Male\n" 
-                "F: Female\n"
-                )
-    sex = str(sex.upper())
-    if sex == "F" or "M":
+    def pick_name():
+        name = input("What is your name?:\n")
+        if 2 < len(name) > 16:
+            print("Naming Rules:\n1: Less than 16 characters and more than 2.")
+            pick_name()
+        Player.name = str(name)
+
+    def pick_sex():
+        sex = input("Pick a sex:\n"
+                    "M: Male\n" 
+                    "F: Female\n"
+                    )
+        sex = str(sex.upper())
         if sex == "F":
             sex = "Female"
-        else:
+            Player.sex = str(sex)
+            return
+        elif sex == "M":
             sex = "Male"
-        Player.sex = str(sex)
-    else:
-        print("Please enter a correct sex 'M' or 'F'")
-        print(sex.upper())
-        return
-    Player.cl = "Villager"
+            Player.sex = str(sex)
+            return
+        else:
+            print("Please enter a correct sex 'M' or 'F'")
+            print(sex.upper())
+            pick_sex()
+
+    def pick_class():
+        print("Pick a class. (Type help for more information)", "Rouge", "Warrior", "Mage", sep='\n', end='\n')
+        Player.cl = input()
+        if Player.cl == "help":
+            print("Classes are a job and the way you will live out your life in the world")
+            pick_class()
+
+
+    #Call the functions to create player
+    pick_name()
+    pick_sex()
+    pick_class()
     Player.level = 1
     Game.pls = [Player]
     Player.stats = [Player.name, Player.sex, Player.cl, Player.level]
@@ -33,8 +53,9 @@ class Player:
         self. sex = sex
         self.cl = cl
         self.level = level
-    stats = []
+        self.maxHP = level * hpMod
 
+    stats = []
 
 class Game:
 
@@ -48,14 +69,13 @@ class Game:
 
     def get_players_info(self):
         for x in self.pls:
-            print(x.name)
+            print('Welcome ' + x.name)
             self.gcr += x.level
 
     def game_start(self):
-        g = Game(1, 0, 0)
+        g = Game(floor=1, deaths=0, kills=0)
         self.get_players_info(Game)
         print("Floor: " + str(g.floor))
-        print("Game start")
         self.encounter(Game)
 
     def encounter(self):
@@ -63,22 +83,23 @@ class Game:
 
 
     class Monster:
-        def __init__(self):
-            self.health = 10
-            self.dmg = 2
-            self. loot = []
+        def __init__(self, health, dmg):
+            self.health = health
+            self.dmg = dmg
+            self.loot = []
             self.level = self.health + self.dmg
 
         def monster_gen(self):
-            m = Game.Monster()
+            m = Game.Monster(random.randint(1, Player.stats[3]), 2)
             #Buffing from Player Callenge Rating
-            m.health *= Game.gcr
-            m.dmg += Game.gcr
+            m.health *= m.level
+            m.dmg += m.level
 
-            for a in m.__dict__.items():
+            for a, b in m.__dict__.items():
                 if a == ('loot', []):
                     continue
-                print(a)
+                print(a.capitalize() + ':', str(b))
+
 
     # Start game
     def setup_game(self):
@@ -97,6 +118,6 @@ if __name__ == '__main__':
         if p == Player.level:
             print("Level: " + str(p))
             continue
-        print(p)
+        print(p.capitalize(), end=' ')
 
 
