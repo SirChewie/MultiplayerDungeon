ddf9c5a1dfaf3bf37c2982c9997d07be486f0796
EyeceScream <jhb1085@gmail.com>
2021-11-28 10:28:03 -0600

    Working on combat. Introducing player and mob speed for turn order.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # Player creation
 import random
+import sys
 
 
 def player_creator():
+    global p
     def pick_name():
         name = input("What is your name?:\n")
         if 2 < len(name) > 16:
             print("Naming Rules:\n1: Less than 16 characters and more than 2.")
             pick_name()
-        Player.name = str(name)
+        p.name = str(name)
 
     def pick_sex():
         sex = input("Pick a sex:\n"
@@ -18,44 +20,110 @@ def player_creator():
         sex = str(sex.upper())
         if sex == "F":
             sex = "Female"
-            Player.sex = str(sex)
-            return
+            p.sex = str(sex)
+
         elif sex == "M":
             sex = "Male"
-            Player.sex = str(sex)
-            return
+            p.sex = str(sex)
+
         else:
             print("Please enter a correct sex 'M' or 'F'")
             print(sex.upper())
             pick_sex()
 
     def pick_class():
-        print("Pick a class. (Type help for more information)", "Rouge", "Warrior", "Mage", sep='\n', end='\n')
-        Player.cl = input()
-        if Player.cl == "help":
+        clist = ["Rogue", "Warrior", "Mage"]
+        print("Pick a class. (Type help for more information)", "Rogue", "Warrior", "Mage", sep='\n', end='\n')
+        c = input()
+        c = c.capitalize()
+        if c == "Help":
             print("Classes are a job and the way you will live out your life in the world")
             pick_class()
+        elif (c in clist):
+            p.cl = c
+            build_class()
+        else:
+            print("Please enter a proper class")
+            pick_class()
+
+    def build_class():
+        if p.cl == 'Rogue':
+            p.level = 1
+            p.hpMod = 30
+            p.dmgMod = 3
+            p.Equ['wepDmg'] = 2
+            p.Inv = []
+            p.maxHP = + (p.level * p.hpMod)
+            p.dmg = p.Equ['wepDmg'] * p.dmgMod
+            p.entType = 'player'
+            p.stats = {'Name': p.name, 'Sex': p.sex, 'Class': p.cl, 'Level': p.level, 'Health': p.maxHP, 'Damage': p.dmg}
+        elif p.cl == 'Warrior':
+            p.level = 1
+            p.hpMod = 50
+            p.dmgMod = 1
+            p.Equ['wepDmg'] = 1
+            p.Inv = []
+            p.maxHP = + (p.level * p.hpMod)
+            p.dmg = p.Equ['wepDmg'] * p.dmgMod
+            p.entType = 'player'
+            p.stats = {'Name': p.name, 'Sex': p.sex, 'Class': p.cl, 'Level': p.level, 'Health': p.maxHP, 'Damage': p.dmg}
+        elif p.cl == 'Mage':
+            p.level = 1
+            p.hpMod = 10
+            p.dmgMod = 5
+            p.Equ['wepDmg'] = 4
+            p.Inv = []
+            p.maxHP = + (p.level * p.hpMod)
+            p.dmg = p.Equ['wepDmg'] * p.dmgMod
+            p.entType = 'player'
+            p.stats = {'Name': p.name, 'Sex': p.sex, 'Class': p.cl, 'Level': p.level, 'Health': p.maxHP, 'Damage': p.dmg}
+        else:
+            p.level = 1
+            p.hpMod = 10
+            p.dmgMod = 1
+            p.Equ['wepDmg'] = 1
+            p.Inv = []
+            p.maxHP = + (p.level * p.hpMod)
+            p.dmg = p.Equ['wepDmg'] * p.dmgMod
+            p.entType = 'player'
+            p.stats = {'Name': p.name, 'Sex': p.sex, 'Class': p.cl, 'Level': p.level, 'Health': p.maxHP, 'Damage': p.dmg}
 
 
     #Call the functions to create player
+
     pick_name()
     pick_sex()
     pick_class()
-    Player.level = 1
-    Game.pls = [Player]
-    Player.stats = [Player.name, Player.sex, Player.cl, Player.level]
+    build_class()
+
+    Game.pls = [p]
+
 
 
 #Player Object Template
 class Player:
-    def __init__(self,  name, sex, cl, level):
+    def __init__(self,  name, sex, cl, level, hpMod, maxHP, dmgMod, dmg, entType):
         self.name = name
         self. sex = sex
         self.cl = cl
         self.level = level
-        self.maxHP = level * hpMod
+        self.hpMod = hpMod
+        self.dmgMod = dmgMod
+        self.maxHP = maxHP
+        self.dmg = dmg
+        self.entType = entType
+    Equ = {}
+    Inv = []
+    stats = {}
 
-    stats = []
+
+class Skills:
+
+    def stab(self):
+        name = "Stab"
+        Game.m.health -= Game.damage_calc(Game, p.dmg)
+        print(p.name + " used " + name)
+
 
 class Game:
 
@@ -65,7 +133,6 @@ class Game:
         self.floor = floor
     pls = []
     gcr = 0
-    m = None
 
     def get_players_info(self):
         for x in self.pls:
@@ -79,27 +146,56 @@ class Game:
         self.encounter(Game)
 
     def encounter(self):
-        self.Monster.monster_gen(Game)
+        self.monster_gen(Game)
+        self.show_stats(Game.m)
+        pturn = False
+        if p.speed > Game.m.speed:
+            pturn = True
+        elif p.speed < Game.m.speed:
+            pturn = False
+        #Prompt the player with there turn
 
+
+    def combat_turn(self):
+        return
+
+    def monster_gen(self):
+        Game.m = self.Monster(random.randint(1, p.stats['Level'] + 1), 2, entType='mob')
+        Game.m.name = 'Monster'  # Placeholder for random monster name picker
+        # Buffing from Player Challenge Rating
+        Game.m.health *= Game.m.level
+        Game.m.dmg += Game.m.level
+
+
+    def show_stats(t):
+        print("-" * 16)
+        # t is the targeted object
+        if t.entType == "player":
+            print(t.name + "'s" + " Stats")
+            for a, b in p.stats.items():
+                print(str(a), end=': ' + str(b) + '\n')
+
+        elif t.entType == 'mob':
+            print(t.name + "'s" + " Stats")
+            for a, b in Game.m.__dict__.items():
+                if a == 'loot':
+                    continue
+                elif a == 'entType':
+                    continue
+                print(a.capitalize() + ':', str(b))
+        print("-" * 16)
+
+    #calculates the game
+    def damage_calc(self, d):
+        return d  #Place holder for more complex dmg calc
 
     class Monster:
-        def __init__(self, health, dmg):
+        def __init__(self, health, dmg, entType):
             self.health = health
             self.dmg = dmg
             self.loot = []
             self.level = self.health + self.dmg
-
-        def monster_gen(self):
-            m = Game.Monster(random.randint(1, Player.stats[3]), 2)
-            #Buffing from Player Callenge Rating
-            m.health *= m.level
-            m.dmg += m.level
-
-            for a, b in m.__dict__.items():
-                if a == ('loot', []):
-                    continue
-                print(a.capitalize() + ':', str(b))
-
+            self.entType = entType
 
     # Start game
     def setup_game(self):
@@ -108,16 +204,20 @@ class Game:
 
 #New player
 def new_player():
+    global p
+    #Clear everything
+    p = Player('', '', '', 0, 0, 0, 0, 0, 'player')
+    p.Equ = {}
+    p.Inv = []
+    p.stats = {}
+    #Start Character creation
     player_creator()
 
 
 if __name__ == '__main__':
     new_player()
     Game.game_start(Game)
-    for p in Player.stats:
-        if p == Player.level:
-            print("Level: " + str(p))
-            continue
-        print(p.capitalize(), end=' ')
+    Game.show_stats(p)
+
 
 
